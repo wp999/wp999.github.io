2e6f76ff529c8de44f4d41cf028e9e19aa6ee8c6
wangpeng <general.wp.1215@outlook.com>
2021-07-13 21:51:58 +0800

    modify

--- miao/lodash/wp999-lodash.js
+++ miao/lodash/wp999-lodash.js
@@ -18,19 +18,23 @@ var wp999 = new (function () {
   this.concat = (arr, ...arrays) => {
     return arr.concat(...arrays)
   }
-  //返回第二个参数
+  //返回第一个数组与第二个多参数数组的差集,数组内容必须是基本类型
   this.difference = (arr1, ...arr2) => {
-    let set = new Set(this.concat(...arr2))
+    let set = new Set(this.concat([], ...arr2))
     return arr1.filter((e) => !set.has(e))
   }
+  //指定深度展开多维数组
   this.flattenDeepth = (arr, i) => {
     if (i === 0) return arr
     return arr.reduce((pre, e) => {
       return pre.concat(Array.isArray(e) ? this.flattenDeepth(e, i - 1) : e)
     }, [])
   }
+  //展开一次多维数组
   this.flatten = (arr) => this.flattenDeepth(arr, 1)
+  //全部展开多维数组
   this.flattenDeep = (arr) => this.flattenDeepth(arr, Infinity)
+  //根据函数调用结果对arr进行分组,如果arr是对象数组,还可以指定属性名进行分组
   this.groupBy = (arr, iteratee) => {
     return arr.reduce((res, e) => {
       let k = this.iter(iteratee)(e)
@@ -39,6 +43,7 @@ var wp999 = new (function () {
       return res
     }, {})
   }
+  //一个简单的迭代器
   this.iter = (e) => {
     if (typeof e === 'string') {
       return (a) => a[e]
@@ -54,8 +59,6 @@ var wp999 = new (function () {
   //从数组右边删除指定数量的元素
   this.dropRight = (arr, num = 1) => {
     if (arr.length <= num) return []
-    return  arr.slice(0,  arr.length - num )
+    return arr.slice(0, arr.length - num)
   }
 })()
-
-
